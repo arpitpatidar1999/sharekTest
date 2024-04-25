aa5a242b2b84681ea3ce2e94c06e2db5275305c9
Arpit Patidar <arpit.patidar@bertlabs.com>
2024-04-25 10:28:16 +0530

    One Stop card created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import { Route } from "react-router-dom";
 import AllRoute from "./Routes/AllRoute";
 import { BrowserRouter } from "react-router-dom";
+import OneStop from "./Components/Cards/OneStop/OneStop";
+
 function App() {
   return (
     <BrowserRouter>
       <AllRoute />
     </BrowserRouter>
+    // <OneStop/>
   );
 }
 
---
+++ src/Components/Cards/OneStop/OneStop.jsx
@@ -0,0 +1,193 @@
+import React from "react";
+import { Box, Button } from "@mui/material";
+
+const OneStop = () => {
+  return (
+    <Box
+      sx={{
+        width: "80%",
+        height: "661px",
+        backgroundColor: "wheat",
+        padding: "36px 63px",
+        margin: "20px",
+        borderRadius: "20px",
+      }}
+    >
+      <Box sx={{ fontSize: "36px", fontWeight: 500, textAlign: "center" }}>
+        One stop for all your automobile needs
+      </Box>
+      <Box
+        sx={{
+          fontSize: "20px",
+          fontWeight: 450,
+          textAlign: "center",
+          marginTop: "26px",
+        }}
+      >
+        Powerful, self-serve product and growth analytics to help you convert,
+        engage, and retain more users. Trusted by over 4,000 startups.
+      </Box>
+      <Box display={"flex"} marginTop={"22px"} justifyContent={"space-between"}>
+        <Box
+          sx={{
+            p: "0 14.93px",
+            backgroundColor: "#FAFCFF",
+            borderRadius: "8px",
+            width: "25%",
+            height: "347px",
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)"
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "40px",
+              height: "40px",
+              backgroundColor: "black",
+              borderRadius: "50%",
+              color: "white",
+              fontSize: "21.85px",
+              fontWeight: 500,
+              marginTop: "27px",
+            }}
+          >
+            01
+          </Box>
+          <Box
+            sx={{
+              fontSize: "20px",
+              fontWeight: 700,
+              lineHeight:"42px",
+              marginTop: "26px",
+            }}
+          >
+            Find the perfect car to for your next staycation
+          </Box>
+          <Box sx={{
+              fontSize: "16px",
+              fontWeight: 450,
+              lineHeight:"24px",
+              marginTop: "26px",
+            }}>
+            Whether you have a team of 2 or 200, our shared team inboxes keep
+            everyone on the same page and in the loop.
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            p: "0 14.93px",
+            backgroundColor: "#FAFCFF",
+            borderRadius: "8px",
+            width: "25%",
+            height: "347px",
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)"
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "40px",
+              height: "40px",
+              backgroundColor: "black",
+              borderRadius: "50%",
+              color: "white",
+              fontSize: "21.85px",
+              fontWeight: 500,
+              marginTop: "27px",
+            }}
+          >
+            02
+          </Box>
+          <Box
+            sx={{
+              fontSize: "20px",
+              fontWeight: 700,
+              lineHeight:"42px",
+              marginTop: "26px",
+            }}
+          >
+            Find the perfect car to try before you buy
+          </Box>
+          <Box sx={{
+              fontSize: "16px",
+              fontWeight: 450,
+              lineHeight:"24px",
+              marginTop: "26px",
+            }}>
+            Whether you have a team of 2 or 200, our shared team inboxes keep
+            everyone on the same page and in the loop.
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            p: "0 14.93px",
+            backgroundColor: "#FAFCFF",
+            borderRadius: "8px",
+            width: "25%",
+            height: "347px",
+            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)"
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "40px",
+              height: "40px",
+              backgroundColor: "black",
+              borderRadius: "50%",
+              color: "white",
+              fontSize: "21.85px",
+              fontWeight: 500,
+              marginTop: "27px",
+            }}
+          >
+            03
+          </Box>
+          <Box
+            sx={{
+              fontSize: "20px",
+              fontWeight: 700,
+              lineHeight:"42px",
+              marginTop: "26px",
+            }}
+          >
+            Find the perfect car to for your next staycation
+          </Box>
+          <Box sx={{
+              fontSize: "16px",
+              fontWeight: 450,
+              lineHeight:"24px",
+              marginTop: "26px",
+            }}>
+            Whether you have a team of 2 or 200, our shared team inboxes keep
+            everyone on the same page and in the loop.
+          </Box>
+        </Box>
+      </Box>
+      <Box align={"center"}>
+        <Button
+          sx={{
+            height: "44px",
+            width: "293px",
+            backgroundColor: "#1F1A1C",
+            color: "#FFFFFF",
+            fontSize: "18px",
+            textTransform: "none",
+            fontWeight: 500,
+            margin: "46px 0 36px 0",
+          }}
+        >
+          Browse Cars
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default OneStop;
